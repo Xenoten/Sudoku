ce14155ee7812f7791849dd198d87580865f862d
Joel Qvint <bmp13jqv@student.lu.se>
2018-02-27 13:48:47 +0100

    Sudoku class added

---
+++ Sudoku/src/sudoku/Sudoku.java
@@ -0,0 +1,21 @@
+package sudoku;
+
+import java.util.ArrayList;
+
+public class Sudoku<E> {
+	ArrayList<ArrayList<E>> matrix;
+	
+	public Sudoku() {
+		matrix = new ArrayList<ArrayList<E>>();
+	}
+	
+	public get
+	
+	public set
+	
+	public solve
+	
+	
+	
+	
+}
