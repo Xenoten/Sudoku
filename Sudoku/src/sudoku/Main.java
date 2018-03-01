217a4ba8a95a7ba4265aff94d91e75c1b7facebf
Erik <Erik@192.168.1.40>
2018-03-01 20:27:13 +0100

    En början på ett bräde

--- Sudoku/src/sudoku/Main.java
+++ Sudoku/src/sudoku/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Sudoku<Integer> spel = new Sudoku();
+		spel.set(1, 1, 1);
+		spel.set(1, 2, 2);
+		spel.set(1, 3, 3);
+		spel.set(1, 4, 4);
+		spel.set(1, 5, 5);
+		spel.set(1, 6, 6);
+		spel.set(1, 7, 7);
+		spel.show();
 
 	}
 
