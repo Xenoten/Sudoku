e8fc4846bc696f62604c4fd1d18e8e07308654fd
Erik <Erik@192.168.1.40>
2018-03-01 21:34:48 +0100

    ett bräde

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -1,21 +1,68 @@
 package sudoku;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Sudoku<E> {
 	ArrayList<ArrayList<E>> matrix;
 	
 	public Sudoku() {
 		matrix = new ArrayList<ArrayList<E>>();
+		for (int i = 0 ; i < 9; i++) {
+			ArrayList<E> e = new ArrayList<E>(Collections.nCopies(9, null));
+			matrix.add(e);
+		}
 	}
 	
-	public get
+	public E get (int col, int row) {
+		return matrix.get(col).get(row);
+	}
 	
-	public set
+	public boolean set (E arg, int col, int row) {
+//		if (matrix.isEmpty()) {
+//			 ArrayList<E> e = new ArrayList<E>(9);
+//			matrix.add(e);
+//		}
+		matrix.get(col).add(row -1 , arg);
+		return true;
+	}
 	
-	public solve
+	public boolean isSolved () {
+		boolean result = true;
+		for (ArrayList<E> a : matrix) {
+			Set<E> temp = new HashSet();
+			for (E e :  a) {
+				temp.add(e);
+				}
+				if (temp.size() != 9) {
+				result = false;
+				}
+			}
+		return result;
+	}
 	
+	public void show () {
+		for (ArrayList<E> a : matrix) {
+			for (E e :  a) {
+				if (e != null) {
+				System.out.print(e);
+				}
+				else System.out.print("  ");
+			}
+			System.out.print("\n");
+		}
+	}
 	
-	
-	
+	public boolean solve() {
+		for (ArrayList<E> a : matrix) {
+			for (E e :  a) {
+				if (e == null) {
+					
+				}
+			}
+		}
+		return true;
+	}
 }
